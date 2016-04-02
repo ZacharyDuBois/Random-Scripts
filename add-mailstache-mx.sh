0b60d029f7141cbfd26e350c3cf6116cc147c6c5
Zachary DuBois <me@zacharydubois.me>
2016-04-02 16:24:28 -0400

    Update Mailstache adder to use DOCTL.

--- add-mailstache-mx.sh
+++ add-mailstache-mx.sh
@@ -15,12 +15,27 @@
 auth="YOURAPIV2KEYHERE"
 
 
-echo -n "Domain name: "
-read domain
-curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $auth" -d '{"type":"MX","data":"mx.mailstache.io.","priority":1}' "https://api.digitalocean.com/v2/domains/$domain/records" > /dev/null
-curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $auth" -d '{"type":"MX","data":"mx2.mailstache.io.","priority":5}' "https://api.digitalocean.com/v2/domains/$domain/records" > /dev/null
-curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $auth" -d '{"type":"MX","data":"mx3.mailstache.io.","priority":5}' "https://api.digitalocean.com/v2/domains/$domain/records" > /dev/null
-curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $auth" -d '{"type":"MX","data":"mx4.mailstache.io.","priority":10}' "https://api.digitalocean.com/v2/domains/$domain/records" > /dev/null
-curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $auth" -d '{"type":"MX","data":"mx5.mailstache.io.","priority":10}' "https://api.digitalocean.com/v2/domains/$domain/records" > /dev/null
-echo "Added Mailstache to $domain"
+
+
+add='true'
+while [[ $add == 'true' ]]
+do
+  doctl compute domain list --format 'Domain'
+  echo -n "Domain name: "
+  read domain
+  doctl compute domain records create $domain --record-type 'MX' --record-data 'mx.mailstache.io.' --record-priority 1
+  doctl compute domain records create $domain --record-type 'MX' --record-data 'mx2.mailstache.io.' --record-priority 5 --no-header
+  doctl compute domain records create $domain --record-type 'MX' --record-data 'mx3.mailstache.io.' --record-priority 5 --no-header
+  doctl compute domain records create $domain --record-type 'MX' --record-data 'mx4.mailstache.io.' --record-priority 10 --no-header
+  doctl compute domain records create $domain --record-type 'MX' --record-data 'mx5.mailstache.io.' --record-priority 10 --no-header
+  echo "Added Mailstache to $domain"
+  echo "Add another?"
+  read -p "[Y/n]: " -n 1 -r confirm
+  echo
+  if [[ ! $confirm =~ ^[Yy]$ ]]
+  then
+    add='false'
+  fi
+done
+
 exit 0
